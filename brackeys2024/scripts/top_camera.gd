fc804ee3c68ffbe1baf1d8d16454d4be1bad859b
Cryogenic <cryogenic.vlog@gmail.com>
2024-09-10 18:50:17 -0700

    top camera fix

--- brackeys2024/scripts/top_camera.gd
+++ brackeys2024/scripts/top_camera.gd
@@ -2,7 +2,7 @@ extends Camera3D
 
 var zoom_speed: float = 1.0
 var follow_speed: float = 5.0
-var camera_distance: float = 100.0
+var camera_distance: float = 200.0
 var zoom_limit: Array = [100.0,3000.0]
 @export var player_node: RigidBody3D
 
@@ -14,5 +14,7 @@ func _physics_process(delta):
 
 func follow_player(delta):
 	if player_node:
-		look_at(player_node.global_position)
-		global_position.y = player_node.global_position.y + camera_distance
+		
+		global_position.x = player_node.global_position.x
+		global_position.z = player_node.global_position.z
+		global_position.y = camera_distance
