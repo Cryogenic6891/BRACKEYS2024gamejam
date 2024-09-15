a3b26ab5bf70ebf024379245ddddd705b3ffd859
Bogzby <62814549+Bogzby@users.noreply.github.com>
2024-09-14 23:23:45 -0700

    commit hope it works

--- brackeys2024/scripts/harbour.gd
+++ brackeys2024/scripts/harbour.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
-@onready var light_pivot: Node3D = $Lighthouse/Light_Pivot
-
+@onready var light_pivot: Node3D = %Light_Pivot
+var tween
 
 func _ready() -> void:
-	var tween = create_tween()
-	tween.tween_property(light_pivot, "rotation", Vector3(0, 0, -90), 90).as_relative()
+	tween = create_tween().set_loops()
+	
+	tween.tween_property(light_pivot, "rotation", Vector3(0.0, 0.0, -90.0), 45).as_relative()
+	print(light_pivot)
